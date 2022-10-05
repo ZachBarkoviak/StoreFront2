61aa5af65a171f5d5bdb97f8352a3038e734cd4c
ZachBarkoviak <zjbarkoviak@outlook.com>
2022-10-05 14:15:58 -0500

    Add project files.

---
+++ StoreFront2.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreFront2.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
